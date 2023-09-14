9e34aa338b1aca8c8b434b9143b535ef99a2da83
Bozhenko, K. (Kateryna) <kateryna.bozhenko@ru.nl>
2023-09-14 17:34:44 +0200

    Replace MinMaxPlayer.py

---
+++ Players/MinMaxPlayer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 14 15:45:02 2023
+
+@author: kateryna
+"""
+
+class MinMaxPlayer(PlayerController):
+    depth = 0
+    def __init__(self, playerID, depth, gameN, heuristic):
+        self.playerID = playerID
+        self.depth = depth
+        self.gameN = gameN
+        self.heuristic = heuristic
+        
+    def makeMove(self, board): 
+        maxValue = minValue #??
+        maxMove = 0
+        for i in range(board.width):
+            if board.isValid(i): 
+                new_board = board.getNewBoard(i, self.playerID) #that's how we make new board?
+                value = self.heuristic.evaluateBoard(self.playerID, new_board) #evaluate that new board to get heuristic value from it
+                if value > maxValue:
+                    maxMove = 1
+                    
+        self.heuristic.getBestAction(self.playerId, board)
+        return maxMove
+            
+#undefined names come with self, right?              
+        
