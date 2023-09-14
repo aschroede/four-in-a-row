1e895738aac8a42676c144de2df99ecc8e398178
Andrew Schroeder <aschroed@ualberta.ca>
2023-09-14 17:23:53 +0200

    More updates to Board, still not done :'(

--- Board.py
+++ Board.py
@@ -1,4 +1,23 @@
 
+# ---- Indexing a 2D Array ------------
+# input = ([[1, 2], [3, 4], [5, 6]])
+# input[0][1] = 2
+# First number is for the row, second number is for the column. This is the same as in Java
+
+# ---- Dimensions of 2D Array ---------
+# numrows = len(input)
+# numcols = len(input[0])
+# 
+
+
+#          -------------Columns-------------
+#              1   2   3   4   5   6   7  
+#----Row 0   |   |   |   |   |   |   |   |
+#----Row 1   |   |   |   |   |   |   |   |
+#----Row 2   |   |   |   |   |   |   |   |
+#----Row 3   |   | o |   | x |   |   |   |
+#----Row 4   |   | o |   | x |   |   |   |
+#----Row 5   |   | o | 0 | x |   |   |   |
 
 
 class Board:
@@ -32,7 +51,24 @@ class Board:
     
     # Get cloned int array of the board state
     def getBoardState(self):
-        return 
+        raise NotImplementedError
+        #TODO
+
+    # let player PlayerId make a move in column x
+    def play(self, col, playerId):
+        # Iterate through each row
+        for row, value in reversed(list(enumerate(self.boardState))):
+            if(self.boardState[row][col] == 0):
+                self.boardState[row][col] = playerId
+                return True
+        
+        return False
+    
+    # Returns if a move is valid by checking if the first row of the specified column is occupied
+    def isValid(self, x):
+        return self.boardState[0][x] == 0
+
+
 
 
     
