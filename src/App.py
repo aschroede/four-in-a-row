22ae7a2bc1fe3af5b8c8908493f3df4459f3a6c5
Kateryna <kateryna@wlan-034106.eduroam.tue.nl>
2023-09-18 11:55:47 +0200

    New message

--- src/App.py
+++ src/App.py
@@ -24,6 +24,7 @@ class App:
             #heuristic2 = SimpleHeuristic(gameN)
 
             # This is a test
+            #This is another test
 
             human1 = HumanPlayer.HumanPlayer(1, gameN, None)
             human2 = HumanPlayer.HumanPlayer(2, gameN, None)
