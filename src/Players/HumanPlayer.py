9942524a702e41202ed0ece997c590e625c4ed44
Kateryna <kateryna@wlan-034106.eduroam.tue.nl>
2023-09-18 18:43:29 +0200

    some major mistakes

--- src/Players/HumanPlayer.py
+++ src/Players/HumanPlayer.py
@@ -6,26 +6,28 @@ Created on Thu Sep 14 13:58:10 2023
 @author: kateryna
 """
 
-from . import PlayerController
+from PlayerController import PlayerController
 
-class HumanPlayer(PlayerController.PlayerController):
 
+class HumanPlayer(PlayerController):
+    
 
     # Create human player
-    def __init__(self, playerID, gameN, heuristic=None):
-        self.playerID = playerID
-        self.gameN = gameN
-        self.heuristic = heuristic
+    def __init__(self, playerID, gameN, heuristic):
+        
+        super().__init__(playerID)
+        super().__init__(gameN)
+        super().__init__(heuristic)
     
     # Show the human player the current board and ask them for the next move       
     def makeMove(self, board):
         print(board)
 
         if self.heuristic != None:
-            print(f"Heuristic: {self.heuristic} calculated the best move is: {self.heuristic.getBestSction(self.playerID, board)+1}")        
-        
-        column = int(input("Player " + str(self) + "\nWhich column would you like to play in? "))
-        
+            print(f"Heuristic: {self.heuristic} calculated the best move is: {self.heuristic.getBestAction(self.playerID, board)+1}")        
+        #not sure what is referred by this in Java
+        print("Player " + self.playerID + "\nWhich column would you like to play in? ")
+        column = int(input())
         print(f"Selected Column: {column}")
         
         return column - 1 
